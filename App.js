c73829eb4a178c31bc7b9e82b29d52e471bf1717
elsaB2023 <elsabosemark@gmail.com>
2021-02-27 10:13:18 -0800

    tab bar

--- App.js
+++ App.js
@@ -1,21 +1,81 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  SafeAreaView,
+  StyleSheet,
+  ScrollView,
+  View,
+  Text,
+  StatusBar,
+} from 'react-native';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
+import {
+  Header,
+  LearnMoreLinks,
+  Colors,
+  DebugInstructions,
+  ReloadInstructions,
+} from 'react-native/Libraries/NewAppScreen';
+import Icon from 'react-native-vector-icons/Ionicons';
+import {createAppContainer} from 'react-navigation';
+import {createMaterialBottomTabNavigator} from 'react-navigation-material-bottom-tabs';
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+import HomeScreen from "./screens/HomeScreen";
+import ProfileScreen from "./screens/ProfileScreen";
+
+
+// class HomeScreen extends React.Component {
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <Text>HomeScreen</Text>
+//       </View>
+//     )
+//   }
+// }
+
+// class ProfileScreen extends React.Component {
+//   render() {
+//     return (
+//       <View style={styles.container}>
+//         <Text>ProfileScreen</Text>
+//       </View>
+//     )
+//   }
+// }
+
+
+const TabNavigator = createMaterialBottomTabNavigator(
+  {
+    Home: {
+      screen: HomeScreen,
+      navigationOptions: {
+        tabBarIcon: ({ tintColor }) => (
+          <View>
+            <Icon style={[{color: tintColor}]} size={25} name={'ios-home'} />
+          </View>
+        ),
+      }
+    },
+    Profile: {
+      screen: ProfileScreen,
+      navigationOptions: {
+        tabBarIcon: ({ tintColor }) => (
+          <View>
+            <Icon style={[{color: tintColor}]} size={25} name={'ios-person'} />
+          </View>
+        ),
+        activeColor: '#ffffff',
+        inactiveColor: '#a3c2fa',
+        barStyle: { backgroundColor: '#2163f6' },
+      }
+    },
   },
-});
+  {
+    initialRouteName: 'Home',
+    activeColor: '#ffffff',
+    inactiveColor: '#bda1f7',
+    barStyle: { backgroundColor: '#6948f4' },
+  }
+);
+
+export default createAppContainer(TabNavigator);
